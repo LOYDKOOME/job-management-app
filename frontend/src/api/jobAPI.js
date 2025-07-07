cd2a97ec8f2d2d1eb944a74776dd2047df02931a
LOYDKOOME <loydkooome@gmail.com>
2025-07-07 13:58:15 +0300

    Connected frontend to live backend

--- frontend/src/api/jobAPI.js
+++ frontend/src/api/jobAPI.js
@@ -18,4 +18,5 @@ export const updateJob = (id, job) => axios.put(`${API_URL}${id}/`, job);
 
 export const deactivateJob = (id) => axios.patch(`${API_URL}${id}/deactivate/`);
 
-export const createJob = (job) => axios.post(API_URL, job);
+export const createJob = (job) =>
+  axios.post("https://job-backend-0jq9.onrender.com/api/jobs/", job);
